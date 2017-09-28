13cb4efea9ced73446b898926ca79969bb43740e
Nicolás M. Pardo <nicolasm@nikodermus.media>
2017-09-27 20:10:25 -0500

    Pull for Diego checkup

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,3 @@
 class Game < ApplicationRecord
+    enum difficulty: %w(easy normal hard)
 end
--- db/migrate/20170927025517_create_games.rb
+++ db/migrate/20170927025517_create_games.rb
@@ -2,7 +2,7 @@ class CreateGames < ActiveRecord::Migration[5.1]
   def change
     create_table :games do |t|
       t.integer :score
-      t.enum difficulty: [:easy, :normal, :hard]
+      t.integer :difficulty
       t.time :duration
       t.date :date
       t.string :img_path
