ffacf8b1a751ff6c09bed9dccf31911e213155e4
Nicolás M. Pardo <nicolasm@nikodermus.media>
2017-10-23 21:47:55 -0500

    Removed everything from rails to its own repo, ready for merge

--- db/migrate/20170928015934_add_omniauth_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddOmniauthToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
-    add_column :users, :name, :string
-    add_column :users, :img_path, :text
-    add_column :users, :nickname, :string
-  end
-end
