acf1014da1bff9e78198231bf4d0556cf15d1ad8
Nicolás M. Pardo <nicolasm@nikodermus.media>
2017-10-24 11:43:57 -0500

    Corrected webpack resources and scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
-	module.exports = {
-		entry: {
-			bundle: './app/javascripts/game_engine.js',
-		},
-		output: {
-			filename: '[name].js',
-			path: __dirname + '/assets/javascripts/'
-		},
-		module: {
-			rules: [{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['env']
-					}
+module.exports = {
+	entry: {
+		bundle: './app/javascripts/game_engine.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: __dirname + '/assets/javascripts/'
+	},
+	cache: false,
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['env'],
+					minified: false
 				}
-			}]
-		}
-	};
+			}
+		}]
+	},
+	devServer: {
+		compress: false,
+		port: 9000
+	}
+};
